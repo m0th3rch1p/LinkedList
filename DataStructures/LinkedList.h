2e6be642c6669fb799dca489c3bb9b07d67dc9e3
shei25420 <chegeeric66@gmail.com>
2023-01-30 02:30:48 +0300

    0n3

---
+++ DataStructures/LinkedList.h
@@ -0,0 +1,21 @@
+#ifndef Limkedlist_h
+#define Linkedlist_h
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Node.h"
+
+struct LinkedList_int
+{
+    struct Node_int *head;
+    int length;
+
+    void(*insert)(int index, int data, struct LinkedList_int *LinkedList);
+    void(*remove)(int index, struct LimkedList_int *LinkedList);
+    int(*retrieve)(int index, struct LimkedList_int *LinkedList);
+};
+
+
+struct LinkedList_int linked_list_int_constructor();
+
+#endif
