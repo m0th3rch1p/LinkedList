8898b5a0180126ec9a2b03ee4c5fb18cfaa04cba
m0th3rch1p <njagibret@gmail.com>
2023-01-30 20:53:45 +0300

    removed residual object files

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gcc ./DataStructures/Node.c -c Node.o
-gcc ./DataStructures/LinkedList.c -c LinkedList.o
-gcc ./DataStructures/Node.o ./DataStructures/LinkedList.o ./DataStructures/test.c -o test.sh
-echo "Done!"
