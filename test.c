c1a295f90c43b529ad831ae74541b4c12734f4a6
m0th3rch1p <njagibret@gmail.com>
2023-05-11 02:11:48 +0300

    uodated test

--- test.c
+++ test.c
@@ -5,16 +5,19 @@ int main()
 {
     struct LinkedList list = linked_list_constructor();
 
-    for (int i=0; i<10;i++)
+    for (int i = 0; i < 12; i++)
     {
-        int*x=(int *)malloc(sizeof(int));
-        *x=i;
-        list.insert(i,x,&list);
+        int *x = (int *)malloc(sizeof(int));
+        *x = i;
+        list.insert(i, x, &list);
     }
-    list.remove(3,&list);
-    list.remove(7,&list);
+    int *y=(int *)malloc(sizeof(int));
+    *y=99;
+    list.insert(list.length, y, &list);
+    list.remove(3, &list);
+    list.remove(7, &list);
 
-    for (int i=0;i<9;i++)
+    for (int i = 0; i < list.length; i++)
     {
         printf("%d\n", *(int *)list.retrieve(i, &list));
     }
